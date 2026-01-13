69a2bc59ce253eba399a3680e8f1fcc612259542
Justyn Temme <justyntemme@gmail.com>
2026-01-13 17:51:36 -0600

    bugfix: syntax error

--- src/event.zig
+++ src/event.zig
@@ -7,20 +7,30 @@ pub fn handleEvent(app: *state.AppState, event: anytype) void {
                 .char => |c| {
                     switch (c) {
                         '/' => app.mode = .search,
-                        'q' => if (app.mode != .search ) { .app.running = false; } else {},
-                        'k' => if (app.mode != .search ) { .app.up(); } else {},
-                        'j' => if (app.mode != .search ) { .app.down(); } else {},
-                        'u' => {
+                        'q' => if (app.mode != .search) {
+                            app.running = false;
+                        } else {},
+                        'k' => if (app.mode != .search) {
+                            app.up();
+                        } else {},
+                        'j' => if (app.mode != .search) {
+                            app.down();
+                        } else {},
+                        'u' => if (app.mode != .search) {
                             for (0..5) |_| app.up();
-                        },
-                        'd' => {
+                        } else {},
+                        'd' => if (app.mode != .search) {
                             for (0..5) |_| app.down();
-                        },
+                        } else {},
                         else => {},
                     }
                 },
-                //If in search then reset to normal mode, if in normal mode exit program 
-                .esc => if (app.mode != .search) {app.running = false;} else {app.mode = .normal },
+                //If in search then reset to normal mode, if in normal mode exit program
+                .esc => if (app.mode != .search) {
+                    app.running = false;
+                } else {
+                    app.mode = .normal;
+                },
                 .up => {
                     app.up();
                 },
